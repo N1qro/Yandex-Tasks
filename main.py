88a240478c1590285f3ed6e6ede5a3d0c1805704
Andrew <andrey777-novikov777@yandex.ru>
2022-11-19 16:21:52 +0300

    Solved "Espresso" Yandex task

--- main.py
+++ main.py
@@ -1,63 +1,53 @@
 import random
 import sys
+import sqlite3
 
-from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5.QtCore import QRect, QSize, QPoint
+from PyQt5.QtWidgets import QApplication, QWidget, QTableWidgetItem
+from PyQt5.QtCore import QRect, QSize, QPoint, Qt, QVariant
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5 import uic
-from UI import Ui_Form
-
-MIN_RADIUS = 40
-MAX_RADIUS = 60
 
 
-class Window(QWidget, Ui_Form):
+class Window(QWidget):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)
-        self.toDraw = False
-        self.circles = list()
-        self.drawButton.clicked.connect(self.onClick)
+        uic.loadUi('main.ui', self)
+        self.clearButton.clicked.connect(self.onClear)
+        self.refreshButton.clicked.connect(self.onRefresh)
 
-    def onClick(self):
-        geometry = self.rect()
-        topRight = geometry.topRight()
-        topLeft = geometry.topLeft()
+        self.onRefresh()
 
-        randX, randY = random.randint(
-            topLeft.x(), topRight.x()), random.randint(topLeft.y(), topRight.x())
-        radius = random.randint(MIN_RADIUS, MAX_RADIUS)
+    def onClear(self):
+        self.tableWidget.setRowCount(0)
+        self.tableWidget.setColumnCount(0)
+        self.tableWidget.clear()
 
-        Circle = QRect()
-        Circle.setSize(QSize(radius, radius))
-        Circle.moveCenter(QPoint(randX, randY))
+    def onRefresh(self):
+        title = ['grade', 'degree', 'type', 'description', 'price', 'volume']
+        self.tableWidget.setColumnCount(len(title))
+        self.tableWidget.setHorizontalHeaderLabels(title)
+        self.tableWidget.setRowCount(0)
+        for _id, *data in self.getDBInfo():
+            self.tableWidget.setRowCount(
+                self.tableWidget.rowCount() + 1)
+            for j, elem in enumerate(data):
+                item = QTableWidgetItem()
+                item.setData(Qt.ItemDataRole.EditRole, QVariant(elem))
+                self.tableWidget.setItem(
+                    _id - 1, j, item)
 
-        Color = self.getRandomColor()
-        self.circles.append((Circle, Color))
-        self.paint()
+        self.tableWidget.resizeColumnsToContents()
+        self.tableWidget.adjustSize()
 
-    def getRandomColor(self):
-        r = random.randint(0, 255)
-        g = random.randint(0, 255)
-        b = random.randint(0, 255)
-        return QColor(r, g, b)
+    def getDBInfo(self):
+        db = sqlite3.connect('coffee.db')
+        cursor = db.cursor()
 
-    def paintCircles(self, qp: QPainter):
-        for circle, color in self.circles:
-            qp.setBrush(color)
-            qp.drawEllipse(circle)
+        query = """
+            SELECT * FROM info
+        """
 
-    def paint(self):
-        self.toDraw = True
-        self.repaint()
-
-    def paintEvent(self, event):
-        if self.toDraw:
-            qp = QPainter()
-            qp.begin(self)
-            self.paintCircles(qp)
-            qp.end()
-            self.toDraw = False
+        return cursor.execute(query).fetchall()
 
 
 if __name__ == "__main__":
