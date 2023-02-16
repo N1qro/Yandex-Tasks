36e7e0f3eefd2b325dc4df24383edb9a6b301519
Andrew <113201151+N1qro@users.noreply.github.com>
2023-02-16 10:16:10 +0300

    Solved astronaut form task

--- AstronautForm/main.py
+++ AstronautForm/main.py
@@ -1,2 +1,15 @@
-import flask
-app = flask.app(__name__)
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/astronaut_selection')
+def form_view():
+    return render_template("base.html")
+
+@app.route('/')
+def index_view():
+    return "<body>Это базовая страница. Решение находится на /astronaut_selection</body>"
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
