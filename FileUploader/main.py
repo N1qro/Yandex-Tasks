a72d5be57de62aec293ee55b4e2f5158eeb1df92
Andrew <113201151+N1qro@users.noreply.github.com>
2023-02-21 14:23:21 +0300

    Solved FileUploader task

--- FileUploader/main.py
+++ FileUploader/main.py
@@ -1,5 +1,11 @@
-from flask import Flask, render_template
+import os
+
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+
 app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = os.path.join("static", "img")
+app.secret_key = "ASLJKdnkdfjkladfklNFSJKdf;a;dfksadkfL:Asd,.xzc,c,xz.cx.z,aASQWe"
 
 
 @app.route("/")
@@ -7,11 +13,16 @@ def homepage_view():
     return render_template("homepage.html")
 
 
-@app.route("/load_photo")
+@app.route("/load_photo", methods=["GET", "POST"])
 def fileuploader_view():
-    return render_template("fileuploader.html")
-
+    if request.method == "POST":
+        file = request.files["file"]
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+            return render_template("fileuploader.html", filename=os.path.join(app.config["UPLOAD_FOLDER"], filename))
+    return render_template("fileuploader.html", filename=None)
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    app.run(host="127.0.0.1", port=8080)
